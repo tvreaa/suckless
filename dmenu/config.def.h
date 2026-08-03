f29efa79059e2d6efbf51fa69c8ee68aea518ecd
tvreaa <7some@tutamail.com>
2026-08-03 22:50:00 +0400

    Add files via upload

---
+++ dmenu/config.def.h
@@ -0,0 +1,30 @@
+#include "theme/nord.h"
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+
+//static const char *fonts[] = {
+//	"monospace:size=10"
+//};
+
+// see theme/theme.h for fonts and main dmenu colors
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { col_gray3, col_gray1 },
+  [SchemeSel] = { col_gray4, col_cyan },
+	[SchemeSelHighlight] = { "#ffc978", col_cyan },
+	[SchemeNormHighlight] = { "#ffc978", col_gray1 },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
